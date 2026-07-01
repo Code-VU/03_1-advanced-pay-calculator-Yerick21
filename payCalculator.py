b2c4c70a4c675469e7804ed8161ce256ebe350e0
Yerick Gonzalez <ygonz106@wgu.edu>
2026-06-30 20:10:42 -0500

    Succesfully completed all tests for payCalculator exercises

--- payCalculator.py
+++ payCalculator.py
@@ -1,6 +1,28 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+
+    try:
+        hours = input("Enter Hours: ")
+        hours = float(hours)
+
+        hourlyRate = input("Enter Rate: ")
+        hourlyRate = float(hourlyRate)
+        
+    except ValueError:
+        print("Error, please enter numeric input")
+        return
+
+
+    if hours > 40:
+        overtimeHours = hours - 40
+        overtimePay = hourlyRate * 1.5 * overtimeHours
+        hours = hours - overtimeHours
+    else:
+        overtimePay = 0
+    print("Pay:", (hours * hourlyRate) + overtimePay)
+        
+ 
+
     
     # end assignment
 
